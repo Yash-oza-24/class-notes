a861301182a26f390e5beff59262fcdfbfeb41f5
Yash-oza-24 <136308852+Yash-oza-24@users.noreply.github.com>
2024-12-05 10:30:28 +0530

    Update Auth.jsx

--- Frontend/src/Pages/Auth/Auth.jsx
+++ Frontend/src/Pages/Auth/Auth.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import LoginPage from "../../Components/login";
+import LoginPage from "../../Components/Login";
 import ForgotPasswordPage from "../../Components/Forgotpassword";
 
 const Auth = () => {
